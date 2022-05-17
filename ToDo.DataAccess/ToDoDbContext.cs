f3a3e5d6eb14f2f5c2b97589f243a8014a01abcc
Maksim Balandin <Maksim.Balandin@colliers.com>
2022-05-17 17:37:35 +0700

    Added DataAccess layer

---
+++ ToDo.DataAccess/ToDoDbContext.cs
@@ -0,0 +1,14 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+using ToDo.Domain.Models;
+
+namespace ToDo.DataAccess;
+
+public class ToDoDbContext : DbContext
+{
+    public DbSet<ToDoItem> ToDoItems { get; set; }
+
+    public ToDoDbContext(DbContextOptions<ToDoDbContext> options) : base(options)
+    { }
+}

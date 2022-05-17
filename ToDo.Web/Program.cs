f46c0e34bd640d0c12b2219766958699aa8abb61
Maksim Balandin <Maksim.Balandin@colliers.com>
2022-05-17 17:40:35 +0700

    Created ToDoComponents

--- ToDo.Web/Program.cs
+++ ToDo.Web/Program.cs
@@ -1,22 +1,24 @@
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
+using Microsoft.EntityFrameworkCore;
 
-using ToDo.Web.Data;
+using MudBlazor.Services;
+
+using ToDo.DataAccess;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
+
+builder.Services.AddMudServices();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContextFactory<ToDoDbContext>(options => options.UseSqlServer(connectionString));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
